08aca91789a25e87432cf9a404229254f9759af2
email <mastermalaki33@gmail.com>
2017-07-11 14:45:28 -0400

    Added Rock Paper Scizzors, boi

---
+++ RPS.js
@@ -0,0 +1,7 @@
+var playerChoice = "m";
+
+
+var choices = ["m", "n", "o"]
+var compChoice = choices[Math.floor(Math.random()*3)];
+
+
---
+++ condition.js
@@ -0,0 +1,9 @@
+var level = 50;
+
+if (level > 50){
+    console.log("Go Back To Training, Newbie")
+}
+else 
+{
+    console.log("Welcome To The High Ranks")
+}
