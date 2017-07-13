8c246d454f95d7a3d9ab1703e58ee41d5e369dbd
email <mastermalaki33@gmail.com>
2017-07-13 14:09:19 -0400

    Editted Roster

--- pokemon-roster.js
+++ pokemon-roster.js
@@ -1,9 +1,11 @@
-var myPokeRosters=
-["placeholder"[Lucario, "Fighting"&"Steel", 250, 130, 202,false],
-[Zekrom, "Dragon"&"Electric", 310, 220, 220,true],
-[Reshiram, "Dragon"&"Fire", 310, 184, 220,true],
-[Hoopa, "Psychic"&"Ghost", 270, 112, 292,true],
-[Rayquaza, "Dragon"&"Flying", 320, 184, 328,true],
-[Lucario, "Fighting"&"Steel", 250, 130, 202,false]  ,"placeholder"]
-
+var myPokeRosters= [
+["Lucario", "Fighting","Steel", 250, 130, 202,false],
+// [Zekrom, "Dragon","Electric", 310, 220, 220,true],
+// [Reshiram, "Dragon","Fire", 310, 184, 220,true],
+// [Hoopa, "Psychic","Ghost", 270, 112, 292,true],
+// [Rayquaza, "Dragon"&"Flying", 320, 184, 328,true],
+// [Solgaleo, "Psychic"&"Steel", 384, 197, 251,true]
+];
 //Name, Type, HP, Def, Attack, And Legend?
+
+console.log("name:"+myPokeRosters[x][0]);
