031ee970062b64530ecba1f7ddbe8c4e9d5d6556
email <mastermalaki33@gmail.com>
2017-07-14 11:33:56 -0400

    test

---
+++ test.js
@@ -0,0 +1,17 @@
+var myNumber=(17);
+
+var myName = ("Malaki Elijah Martin");
+
+var myArray = [17, "Malaki", true];
+
+function printHello(
+    console.log =[myArray]
+)
+
+//having trouble with conditional here
+if (answer === true){
+    console.log('true')
+}
+
+//mind farting when it comes to loop (which should be easy to remember)
+
