fd7ab234b2ec498d6a8e54709ebf717690720ff7
email <mastermalaki33@gmail.com>
2017-07-17 15:16:22 -0400

    Gotta Folder These

---
+++ picky.js
@@ -0,0 +1,14 @@
+function setup(){
+    createCanvas(600,600)
+    fill("white")
+    arc(300, 300, 500, 500, 0, PI, CHORD);
+    fill("red")
+    arc(300, 300, 500, 500, PI, 0, CHORD);
+    //ellipse(300, 300, 500, 500);
+    fill("white")
+    ellipse(300, 300, 100, 100)
+    
+
+
+
+};
